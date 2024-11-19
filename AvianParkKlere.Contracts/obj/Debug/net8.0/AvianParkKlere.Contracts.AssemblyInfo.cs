033e218e4c4a792f5b72ca1e96e52ea75b802cc2
albert@ankerdata.com <albert@ankerdata.com>
2024-11-19 19:56:09 +0200

    Contracts push

--- AvianParkKlere.Contracts/obj/Debug/net8.0/AvianParkKlere.Contracts.AssemblyInfo.cs
+++ AvianParkKlere.Contracts/obj/Debug/net8.0/AvianParkKlere.Contracts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvianParkKlere.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8d1be08244067aa564baa17bb91b51ce7f0fca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88e3735174784b8b4ede275555a45b8088d08457")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvianParkKlere.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvianParkKlere.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
