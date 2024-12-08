dbf3e98959ce8fc2a3b87cebf5aebfe8ba9fc285
albert@ankerdata.com <albert@ankerdata.com>
2024-12-08 15:33:49 +0200

    Finished View dialogs.

--- AvianParkKlere.Contracts/obj/Debug/net8.0/AvianParkKlere.Contracts.AssemblyInfo.cs
+++ AvianParkKlere.Contracts/obj/Debug/net8.0/AvianParkKlere.Contracts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvianParkKlere.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88e3735174784b8b4ede275555a45b8088d08457")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33fc3bba51c851a20f231267a6cb248cb8fa10cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvianParkKlere.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvianParkKlere.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
